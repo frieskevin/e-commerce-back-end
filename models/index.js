88f508b68bf5d7d64b3730a96e0ac9f370b5936f
alotofsnakes <mrfries@gmail.com>
2022-10-09 18:39:59 -0700

    model columns and relationships

--- models/index.js
+++ models/index.js
@@ -5,12 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'hmm',
+  foreignKey: 'id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'tagsss',
+  foreignKey: 'id'
+})
 
 module.exports = {
   Product,
